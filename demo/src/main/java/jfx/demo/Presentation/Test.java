238d40e05a979677d858407de79a24817752573e
Luis Esteban Robelto Z <luis.robelto@uptc.edu.co>
2023-12-16 11:51:34 -0500

    BinaryList Update

--- demo/src/main/java/jfx/demo/Presentation/Test.java
+++ demo/src/main/java/jfx/demo/Presentation/Test.java
@@ -1,11 +1,35 @@
 package jfx.demo.Presentation;
 
 import Controller.Management;
+import BinaryTreeLogic.BinaryTree;
+
+import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        Management management = new Management();
-        System.out.println(management.generateAscciCode("AA"));
-        System.out.println(management.generatePosition("Zndres"));
+
+        BinaryTree<Integer> bts = new BinaryTree<>((o1,o2)->Integer.compare( o1, o2));
+
+        bts.addNode(5);
+        bts.addNode(1);
+        bts.addNode(6);
+        bts.addNode(2);
+        bts.addNode(9);
+        bts.addNode(3);
+        bts.addNode(-3);
+        bts.addNode(-4);
+        bts.addNode(4);
+
+
+
+        ArrayList<Integer> aux= new ArrayList<>();
+        aux=bts.listPresort();
+
+
+        for(Integer i: aux){
+            System.out.println(i);
+        }
+
+
     }
 }
